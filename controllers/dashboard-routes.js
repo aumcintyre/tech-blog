2b8237e2ee678836b6a956db931d98ceaac6e171
aumcintyre <aumcintyre@gmail.com>
2022-11-07 14:11:58 -0600

    get /login not working

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -36,3 +36,5 @@ router.get('/', withAuth, (req, res) => {
         res.status(500).json(err);
     });
 });
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const homeRoutes = require('./homeRoutes.js');
-// const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const apiRoutes = require('./api');
 
-// const dashboardRoutes = require('./dashboard-routes.js')
+const dashboardRoutes = require('./dashboard-routes.js')
 
 router.use('/', homeRoutes);
-// router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
