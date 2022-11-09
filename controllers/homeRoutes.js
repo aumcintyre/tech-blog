732134336bcd40c33064c1318c6518b225018811
aumcintyre <aumcintyre@gmail.com>
2022-11-09 09:39:41 -0600

    home routes built but untested

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,4 @@
-const sequelize = require ('../config/connnection');
+const sequelize = require('../config/connnection');
 const { Post, User, Comment } = require('../models');
 const router = require('express').Router();
 
@@ -28,14 +28,96 @@ router.get('/', (req, res) => {
             attributes: ['username']
         }]
     })
-    .then(postData => {
-        const posts = postData.map(post => post.get({ plain: true }));
-        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+        .then(postData => {
+            const posts = postData.map(post => post.get({ plain: true }));
+            res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'content', 'title', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_message', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+
+            {
+                model: User,
+                attributes: ['username']
+            }]
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+        .then(postData => {
+            if (!postData) {
+                res.status(404).json({ message: 'No existing post matches this ID.' });
+                return;
+            }
+            const post = postData.get({ plain: true });
+            console.log(post);
+            res.render('single-post', { post, loggedIn: req.session.loggedIn });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/post-comments', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'content', 'title', 'created_at'],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'comment_message', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }]
     })
-})
+        .then(postData => {
+            if (!postData) {
+                res.status(404).json({ message: 'No existing post matches this ID' });
+                return;
+            }
+            const post = postData.get({ plain: true });
+            console.log(post);
+            res.render('post-comments', { post, loggedIn: req.session.loggedIn });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
