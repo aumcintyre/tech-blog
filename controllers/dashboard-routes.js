fd6d38f4c3902002d14e087c65c66d6fab642a30
aumcintyre <aumcintyre@gmail.com>
2022-11-09 13:01:35 -0600

    built out dashboard routes, need script pages

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -22,19 +22,56 @@ router.get('/', withAuth, (req, res) => {
                 model: User,
                 attributes: ['username']
             }
-        },{
+        }, {
             model: User,
             attributes: ['username']
         }]
     })
+        .then(postData => {
+            const posts = postData.map(post => post.get({ plain: true }));
+            res.render('dashboard', { posts, loggedIn: true })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'content', 'created_at'],
+        include: [{
+            model: User,
+            attributes: ['username']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_message', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }]
+    })
     .then(postData => {
-        const posts = postData.map(post => post.get({ plain: true }));
-        res.render('dashboard', { posts, loggedIn: true })
+        if(!postData){
+            res.status(404).json({ messages: 'No existing posts match that ID'});
+            return;
+        }
+        const post = postData.get({ plain: true });
+        res.render('edit-post', { post, loggedIn: true });
     })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
 });
+
+router.get('/new', (req, res) => {
+    res.render('create-post')
+})
 
 module.exports = router;
