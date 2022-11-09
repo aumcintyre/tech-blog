efb12ab5f55c9abd980f945c829a3e437173ad07
aumcintyre <aumcintyre@gmail.com>
2022-11-09 17:39:41 -0600

    login still broken

--- public/js/login.js
+++ public/js/login.js
@@ -1,22 +1,22 @@
 async function loginFormHandler(event) {
-    event.preventDefault();
+    event.preventDefault(event);
 
     const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
 
     if(username && password) {
-        const login = await fetch('/api/users/login', {
+        const response = await fetch('/api/users/login', {
             method: 'post',
             body: JSON.stringify({
                 username,
                 password
             }),
-            headers: { 'Content-Type': 'application.json' }
+            headers: { 'Content-Type': 'application/json' }
         })
-        if (login.ok) {
-            document.location.replace('/dashboard');
+        if (response.ok) {
+            document.location.replace('/dashboard/');
         } else {
-            alert(login.statusText);
+            alert(response.statusText);
         }
     }
 }
